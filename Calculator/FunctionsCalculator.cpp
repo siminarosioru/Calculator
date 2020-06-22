ae620e45d8708d79b633f2f22b04f564cd225be7
siminarosioru <simina.rosioru@gmail.com>
2020-06-22 13:50:02 +0200

    the description of functions

--- Calculator/FunctionsCalculator.cpp
+++ Calculator/FunctionsCalculator.cpp
@@ -3,32 +3,37 @@
 #include <iostream>
 
 
-// implement the functions
+//implement the addition function
 double addition(double a, double b)
 {
 	return a + b;
 }
 
+//implement the minus function
 double minus(double a, double b)
 {
 	return a - b;
 }
 
+//implement the multiplication function
 double multiplication(double a, double b)
 {
 	return a * b;
 }
 
+//implement the division function
 double division(double a, double b)
 {
 	return a / b;
 }
 
+//implement the number function at the second power
 double patrat(double a)
 {
 	return a * a;
 }
 
+//implement the percent function
 double procent(double Op1, double Op2, char Operation)
 {
 	double result = 0.0;
@@ -45,11 +50,13 @@ double procent(double Op1, double Op2, char Operation)
 	return result;
 }
 
+//implement function 1 divided by x
 double ImpartireaLui1Lax(double a)
 {
 	return 1 / a;
 }
 
+//implement the square root function
 double SquareRoot(double a)
 {
 	double result = sqrt(a);
@@ -63,6 +70,7 @@ double CubeRoot(double a)
 	return cbrt(a);
 }
 
+//implement the number function at the third power
 double Cubic(double a)
 {
 	return a * a * a;
